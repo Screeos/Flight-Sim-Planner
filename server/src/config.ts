85653c712fff76f23546402249e6af7a40ea5454
Noah Huppert <contact@noahh.io>
2020-10-28 01:21:15 -0400

    server: got interface validation working

--- server/src/config.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Application configuration file format.
- */
-export interface Config {
-    /**
-	* Port on which the HTTP API server will listen.
-	*/ 
-    httpPort: number
-}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,71 @@
 import { Application } from 'https://deno.land/x/abc@v1.1.0/mod.ts';
-import { createCheckers } from 'https://dev.jspm.io/ts-interface-checker/mod.ts';
-import { MongoClient } from 'https://deno.land/x/mongo@v.013.0/mod.ts';
+import { MongoClient } from 'https://deno.land/x/mongo@v0.13.0/mod.ts';
 import { parse as parseToml } from 'https://deno.land/std@0.74.0/encoding/toml.ts';
+import {
+    validate,
+    flattenMessages,
+    required,
+    isNumber,
+    validateObject,
+} from "https://deno.land/x/validasaur/mod.ts";
 
-import ConfigTI from './config-ti.ts';
+/**
+ * Throws an exception if data does not pass validation.
+ * @param data To validate.
+ * @param schema Schema to test data.
+ * @returns True if data is valid.
+ * @throws string If data is not valid.
+ */
+async function isValid(data: any, schema: any): Promise<boolean> {
+    const [ passes, errs ] = await validate(data, schema);
+    if (passes === false) {
+	   const flatErrs = flattenMessages(errs);
+	   
+	   let msgs = Object.keys(flatErrs).map((key: string) => {
+		  let nameParts = key.split('.');
+		  if (nameParts.length > 1) {
+			 nameParts.splice(nameParts.length - 1, 1);
+		  }
 
-// Setup type guards
-const { ConfigGuard } = createCheckers(configTI);
+		  const name = nameParts.join('.');
+
+		  return `for ${name}: ${flatErrs[key]}`;
+	   });
+	   
+	   throw msgs.join(', ');
+    }
+
+    return true;
+}
 
 // Load configuration
-const cfg = parseToml(await Deno.readTextFile('./config.toml'));
+/**
+ * Application configuration file format.
+ */
+interface Config {
+    /**
+	* Port on which the HTTP API server will listen.
+	*/ 
+    httpPort: number
+}
+
+async function IsConfig(data: any): Promise<Config> {
+    await isValid(data, {
+	   http: validateObject(true, {
+		  httpPort: [ required, isNumber ],
+		  bar: required,
+	   }),
+    });
+    return data as Config;
+}
+
+let cfg: Config = null;
+try {
+    cfg = await IsConfig(parseToml(await Deno.readTextFile('./config.toml')));
+} catch (e) {
+    console.error(`failed to get configuration: ${e}`);
+    Deno.exit(1);
+}
 
 // Define handlers
 const app = new Application();

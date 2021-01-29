8969b104add1b1eff2d10793e8975f7cedc17afc
Noah Huppert <contact@noahh.io>
2021-01-28 19:02:25 -0500

    server: implemented get flight plan endpoint

--- server/main.go
+++ server/main.go
@@ -11,7 +11,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
+	mprimitive "go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	moptions "go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -40,7 +41,7 @@ type Config struct {
 // FlightPlan describes the details of a simulator flight
 type FlightPlan struct {
 	// ID unique identifier
-	ID string
+	ID mprimitive.ObjectID `bson:"_id"`
 
 	// FromAirport ICAO
 	FromAirport string
@@ -82,26 +83,44 @@ type ErrorResp struct {
 	Message string
 }
 
-// RespondErr sends an error response if err is not nil.
-// The response will be a ErrorResp and code 500. The
-// err will be logged.
-// Returns true when a response was sent and endpoint
-// execution should stop. False when no response is sent
-// and everything should continue
-func (a HTTPAPI) RespondErr(c *gin.Context, msg string, err error) bool {
-	if err != nil {
-		c.JSON(http.StatusInternalServerError,
-			ErrorResp{
-				Message: msg,
-			})
+// RespondErr sends a response with code with a body of
+// ErrorResp which contains the msg. The err will never
+// be sent to the user but might be logged. Msg should
+// be written for a user and not start with a capital
+// letter (unless first word is an acronym) or end with
+// any punctuation, use "we" when refering to the server
+// or database ect. For err to be logged the code must
+// be 500 or greater and not err cannot be nil.
+func (a HTTPAPI) RespondErr(c *gin.Context, code int, msg string, err error) {
+	c.JSON(code, ErrorResp{
+		Message: msg,
+	})
+
+	if code >= 500 && err != nil {
 		log.Error().
 			Err(err).
+			Int("http error code", code).
+			Bool("foo", code >= 500).
 			Str("method", c.Request.Method).
 			Str("path", c.Request.URL.Path).
 			Msg(msg)
-		return true
+	}
+}
+
+// CheckRespondErr will use RespondErr if err is not
+// nil. In this case true will be returned, false
+// otherwise. The caller shouldn't continue an endpoint
+// handler if true is returned. If code is -1 then
+// http.StatusInternalServerError is used by default
+func (a HTTPAPI) CheckRespondErr(c *gin.Context, code int, msg string, err error) bool {
+	if code == -1 {
+		code = http.StatusInternalServerError
 	}
 
+	if err != nil {
+		a.RespondErr(c, code, msg, err)
+		return true
+	}
 	return false
 }
 
@@ -118,9 +137,12 @@ func (a HTTPAPI) Start() error {
 	router.Use(cors.New(corsCfg))
 
 	// Routes
-	router.GET("/api/v0/health", a.EPTHealth)
+	router.GET("/api/v0/health",
+		a.EPTHealth)
 	router.POST("/api/v0/flight_plan",
 		a.EPTCreateFlightPlan)
+	router.GET("/api/v0/flight_plan/:id",
+		a.EPTGetFlightPlan)
 
 	return router.Run(a.Config.HTTPHost)
 }
@@ -142,7 +164,7 @@ type EPTHealthResp struct {
 // EPTHealth provides an indication of the APIs
 // current health.
 func (a HTTPAPI) EPTHealth(c *gin.Context) {
-	c.JSON(200, EPTHealthResp{
+	c.JSON(http.StatusOK, EPTHealthResp{
 		Ok: true,
 	})
 }
@@ -168,18 +190,67 @@ func (a HTTPAPI) EPTCreateFlightPlan(c *gin.Context) {
 	// Save mongodb
 	insertRes, err := a.DB.FlightPlans.InsertOne(
 		a.Ctx, req.FlightPlan)
-	sent := a.RespondErr(c,
-		"failed to save in database", err)
-	if sent {
+	if a.CheckRespondErr(c, -1,
+		"we got your flight plan but failed to "+
+			"save it", err) {
 		return
 	}
 
 	// Respond with inserted
 	insertedFP := req.FlightPlan
-	insertedFP.ID = insertRes.InsertedID.(primitive.ObjectID).Hex()
+	insertedFP.ID = insertRes.InsertedID.(mprimitive.ObjectID)
 
-	c.JSON(200, EPTCreateFlightPlanResp{
+	c.JSON(http.StatusOK, EPTCreateFlightPlanResp{
 		FlightPlan: insertedFP,
+	})
+}
+
+// EPTGetFlightPlanResp provides a requested flight plan
+type EPTGetFlightPlanResp struct {
+	// FlightPlan which was requested
+	FlightPlan FlightPlan
+}
+
+// EPTGetFlightPlan retrieves a flight plan who's ID is
+// specified as the id url parameter
+func (a HTTPAPI) EPTGetFlightPlan(c *gin.Context) {
+	// Get requested ID
+	fpID, err := mprimitive.ObjectIDFromHex(
+		c.Param("id"))
+	if a.CheckRespondErr(c, -1,
+		"we failed to read the flight plan ID "+
+			"you specified", err) {
+		return
+	}
+
+	// Query database for flight plan
+	dbRes := a.DB.FlightPlans.FindOne(a.Ctx, bson.D{{
+		"_id", fpID,
+	}})
+	if dbRes.Err() == mongo.ErrNoDocuments {
+		a.RespondErr(c, http.StatusNotFound,
+			"no flight plan with that ID was found",
+			nil)
+		return
+	}
+
+	if a.CheckRespondErr(c, -1,
+		"we ran into trouble looking for "+
+			"flight plans", dbRes.Err()) {
+		return
+	}
+
+	var foundFP FlightPlan
+	if a.CheckRespondErr(c, -1,
+		"a flight plan was found but we ran into "+
+			"trouble processing the results",
+		dbRes.Decode(&foundFP)) {
+		return
+	}
+
+	// Respond
+	c.JSON(http.StatusOK, EPTGetFlightPlanResp{
+		FlightPlan: foundFP,
 	})
 }
 
